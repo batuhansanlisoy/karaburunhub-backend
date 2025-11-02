85073c2177059d48166baddf76283dd32cbdb679
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2025-11-02 20:27:45 +0300

    yapılandırma

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,16 @@
 import type { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "root123",
-      database: "karaburun",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: "./migrations",
