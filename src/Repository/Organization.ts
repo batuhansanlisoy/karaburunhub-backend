d85f4b28006ad08362d854526edec2289f895b63
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2026-01-20 23:39:14 +0300

    yeni konumlar

---
+++ src/Repository/Organization.ts
@@ -0,0 +1,51 @@
+import db, { Knex } from "../db/knex";
+import { Organization } from "../Entity/Organization";
+
+export class OrganizationRepository {
+    private tableName = "organization";
+
+    async single(id: number): Promise<Organization> {
+        const org = await db(this.tableName).where({ id }).first();
+        if (org?.cover && typeof org.cover === "string") {
+            org.cover = JSON.parse(org.cover); // artık servis tarafında cover bir obje
+        }
+        return org;
+    }
+
+    async getAll(): Promise<Organization[]> {
+        return db(this.tableName).select(
+            "organization.id",
+            "organization.category_id",
+            "organization.name",
+            "organization.email",
+            "organization.phone",
+            "organization.content",
+            "organization.website",
+            "organization.cover",
+            "organization.gallery",
+            "organization.address",
+            "organization.latitude",
+            "organization.longitude",
+            "organization.created_at",
+            "organization.updated_at",
+            "organization_category.name as category_name"
+        )
+        .leftJoin("organization_category", "organization.category_id", "organization_category.id");
+    }
+
+    async getByCategoryId(category_id: number): Promise<Organization[]> {
+        return db(this.tableName).where("category_id", category_id);
+    }
+
+    async create(organization: Partial<Organization>, trx?: Knex.Transaction): Promise<number[]> {
+        const query = trx ? trx(this.tableName) : db(this.tableName);
+        return query.insert(organization);
+    }
+
+    async del(id: number, trx?:  Knex.Transaction): Promise<number[]> {
+        if (trx) {
+            return trx(this.tableName).where({ id }).del();
+        }
+        return db(this.tableName).where({ id }).del();
+    }
+}
---
+++ src/Service/Organization.ts
@@ -0,0 +1,61 @@
+import db from "../db/knex";
+import { FileService } from "./File";
+import { Organization } from "../Entity/Organization";
+import { SubcategoryRepository } from "../Repository/Organization/Subcategory";
+import { Subcategory } from "../Entity/Organization/Subcategory";
+import { OrganizationRepository } from "../Repository/Organization";
+
+export class OrganizationService {
+    private repo = new OrganizationRepository();
+    private sub_category_repo = new SubcategoryRepository();
+
+    async single(id: number): Promise<Organization> {
+
+        return this.repo.single(id);
+    }
+
+    async list(category_id?: number): Promise<Organization[]> {
+        if (category_id) {
+
+            return this.repo.getByCategoryId(category_id);
+        }
+
+        return this.repo.getAll();
+    }
+
+    async create(organization: Partial<Organization>, items: number[]): Promise<void> {
+
+        await db.transaction(async (trx) => {
+            const [organization_id] = await this.repo.create(organization, trx);
+
+            for (const  item_id of items) {
+                const sub_category: Partial<Subcategory> = {
+                    item_id, organization_id
+                };
+
+                await this.sub_category_repo.create(sub_category, trx);
+            }
+        });
+    }
+
+    async del(id: number): Promise<void> {
+
+        await db.transaction(async (trx) => {
+            const org = await this.repo.single(id);
+            console.log(org);
+            if (!org) throw new Error("Organizasyon buluanamadı");
+
+            await this.repo.del(id, trx);
+
+            try {
+                if (org.cover) {
+
+                    FileService.delete(org.cover.url);
+                }
+            } catch (error) {
+                throw new Error("dosya silme hatası" + (error as Error).message);
+            }
+        });
+    }
+    
+}
