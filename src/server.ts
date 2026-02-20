ebcf41a196d1cef581f43cea2e02fb33cc1def73
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2026-02-21 02:16:59 +0300

    port change

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+// PORT'un sayı olduğundan emin olalım
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`\n  🚀 Server is running!`);
     console.log(`  > Local:    http://localhost:${PORT}`);
-    console.log(`  > Network:  http://127.0.0.1:${PORT}\n`);
+    console.log(`  > Network:  http://192.168.8.100:${PORT}\n`);
 });
