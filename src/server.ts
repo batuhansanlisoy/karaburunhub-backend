dd596eb3598059ea2b90eb66bcc16a20f37e0f65
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2026-05-27 23:58:05 +0300

    ıvır zıvır işler

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import app from "./app";
 // PORT'un sayı olduğundan emin olalım
 const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`\n  🚀 Server is running!`);
 });
 
-// // Nginx (90s) ve Cloudflare (60s) ile tam uyum için buraya ekliyoruz:
-// server.keepAliveTimeout = 95000; // 95 saniye (milisaniye cinsinden)
-// server.headersTimeout = 96000;   // 96 saniye (keepAliveTimeout'tan 1 saniye büyük olmalı)
+// Nginx (90s) ve Cloudflare (60s) ile tam uyum için buraya ekliyoruz:
+server.keepAliveTimeout = 95000; // 95 saniye (milisaniye cinsinden)
+server.headersTimeout = 96000;   // 96 saniye (keepAliveTimeout'tan 1 saniye büyük olmalı)
